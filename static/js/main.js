8ef57c5face42ec94eae831192785c11d822d5ad
unknown <anwar.sedjat@uit.ac.ma>
2026-04-06 17:14:07 +0100

    siteweb

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+// Main JavaScript for the website
+
+function toggleMenu(menuId) {
+    const menu = document.getElementById(menuId);
+    if (menu) {
+        menu.classList.toggle('hidden');
+    }
+}
+
+// Add custom scripts here
